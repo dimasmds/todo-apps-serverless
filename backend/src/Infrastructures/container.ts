cc55e4c303b478aa9aa9af148d08bfe5428bd031
Dimas Saputra <dimas.maulanads@gmail.com>
2021-11-20 11:07:47 +0700

    create container

---
+++ backend/src/Infrastructures/container.ts
@@ -0,0 +1,61 @@
+import { createContainer, ParameterOption } from 'instances-container';
+import UUIDGenerator from './generator/UUIDGenerator';
+import JwtTokenizeImpl from './tokenize/JwtTokenizeImpl';
+import TodoRepositoryDynamoDB from './repositories/TodoRepositoryDynamoDB';
+import AWSSecretsManager from './security/AWSSecretsManager';
+import TodoCreationUseCase from '../Applications/use_cases/TodoCreationUseCase';
+
+const container = createContainer();
+
+const useCaseParameter: ParameterOption = {
+  injectType: 'destructuring',
+  dependencies: [
+    {
+      name: 'idGenerator',
+      internal: 'IdGenerator',
+    },
+    {
+      name: 'todoRepository',
+      internal: 'TodoRepository',
+    },
+    {
+      name: 'jwtTokenize',
+      internal: 'JwtTokenize',
+    },
+    {
+      name: 'secretManager',
+      internal: 'SecretManager',
+    },
+  ],
+};
+
+/* Infrastructure */
+container.register([
+  {
+    key: 'IdGenerator',
+    Class: UUIDGenerator,
+  },
+  {
+    key: 'JwtTokenize',
+    Class: JwtTokenizeImpl,
+  },
+  {
+    key: 'TodoRepository',
+    Class: TodoRepositoryDynamoDB,
+  },
+  {
+    key: 'SecretManager',
+    Class: AWSSecretsManager,
+  },
+]);
+
+/* UseCase */
+container.register([
+  {
+    key: TodoCreationUseCase.name,
+    Class: TodoCreationUseCase,
+    parameter: useCaseParameter,
+  },
+]);
+
+export default container;
