39ccf86c13d10a19b2b23d0ab97b56475deecdfe
Dimas Saputra <dimas.maulanads@gmail.com>
2021-11-20 23:32:18 +0700

    implement createTodo

--- backend/src/Interfaces/lambda/http/createTodo.ts
+++ backend/src/Interfaces/lambda/http/createTodo.ts
@@ -1,7 +1,42 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import middy from 'middy';
+import { cors } from 'middy/middlewares';
+import { getTokenFromAuthHeader } from '../../../Commons/utils';
+import container from '../../../Infrastructures/container';
+import TodoCreationUseCase from '../../../Applications/use_cases/TodoCreationUseCase';
 
-export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO : Implement creating a new TODO item
-  console.log('');
-  return { statusCode: 201, body: JSON.stringify({ message: 'Create TODO item' }) };
-};
+const todoCreationUseCase = <TodoCreationUseCase> container.getInstance(TodoCreationUseCase.name);
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+      const requestPayload = JSON.parse(event.body);
+      const accessToken = getTokenFromAuthHeader(event.headers.Authorization);
+
+      const item = await todoCreationUseCase.execute({
+        ...requestPayload,
+        accessToken,
+      });
+
+      return {
+        statusCode: 201,
+        body: JSON.stringify({
+          item,
+        }),
+      };
+    } catch (error: any) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: error.message,
+        }),
+      };
+    }
+  },
+);
+
+handler.use(
+  cors({
+    credentials: true,
+  }),
+);
