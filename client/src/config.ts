d33b4f60f2222e8c5f7016995d44020a9840ba14
Dimas Saputra <dimas.maulanads@gmail.com>
2021-11-22 22:38:15 +0700

    make it local first

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,6 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
 const apiId = 'zvtnz4vf1a'
-export const apiEndpoint = `https://${apiId}.execute-api.ap-southeast-1.amazonaws.com/dev`
+export const apiEndpoint = `http://localhost:3001/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
