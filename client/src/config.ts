5be46c52785fd0a666b9063ecd09458f1b9d537a
Dimas Saputra <dimas.maulanads@gmail.com>
2021-11-24 01:26:30 +0700

    using remote url in client

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,7 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
 const apiId = 'zvtnz4vf1a'
 // TODO: Define URL
-export const apiEndpoint = `http://localhost:3001/dev`
+export const apiEndpoint = `https://${apiId}.execute-api.ap-southeast-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
